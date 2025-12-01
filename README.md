731f982dd4a3fb709abcdd498e972edd33866d8d
Vince Park <vincepark@Vinces-iMac.modem>
2025-12-02 01:32:46 +1100

    basic frontend - backend connection established

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Web chat application using WebRTC and Websocketsß
---
+++ src/server.js
@@ -0,0 +1,36 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req,res) => res.render("home"));
+// app.get("/*", (req,res) => res.redirect("/"));
+
+
+
+console.log("hello");
+
+const handleListen = () => console.log('Listening on http://localhost:3000');
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket) {
+    console.log(socket);
+}
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close",() => console.log("Disconnected from the Browser!"));
+    socket.on("message", (message) => {
+        console.log(message.toString());
+    });
+    socket.send("hello!");
+});
+
+server.listen(3000, handleListen);
